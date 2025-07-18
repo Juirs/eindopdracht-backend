d48b4cf7a89a5b36f98416d3f56284f536ead1ab
Ahmed Bestun <zeroaiko@live.com>
2025-07-18 15:36:12 +0200

    Replaced Dto with entity as I removed password in the UserResponseDto for security reasons, and included a UsernameNotFoundException

--- src/main/java/com/example/eindopdrachtbackend/services/CustomUserDetailsService.java
+++ src/main/java/com/example/eindopdrachtbackend/services/CustomUserDetailsService.java
@@ -1,17 +1,17 @@
 package com.example.eindopdrachtbackend.services;
 
-import com.example.eindopdrachtbackend.dtos.UserResponseDto;
 import com.example.eindopdrachtbackend.models.Role;
+import com.example.eindopdrachtbackend.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -23,17 +23,24 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) {
-        UserResponseDto userResponseDto = userService.getUser(username);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserForAuthentication(username);
 
-        String password = userResponseDto.getPassword();
-        Set<Role> roles = userResponseDto.getRoles();
+        String password = user.getPassword();
+        Set<Role> roles = user.getRoles();
 
         List<GrantedAuthority> grantedRoles = new ArrayList<>();
-        for (Role role: roles) {
-            grantedRoles.add(new SimpleGrantedAuthority(role.getRole()));
+        for (Role role : roles) {
+            grantedRoles.add(new SimpleGrantedAuthority("ROLE_" + role.getRole()));
         }
 
-        return new org.springframework.security.core.userdetails.User(username, password, grantedRoles);
+        return new org.springframework.security.core.userdetails.User(
+                username
+                ,password
+                ,user.getEnabled()
+                ,true
+                ,true
+                ,true
+                ,grantedRoles);
     }
 }
